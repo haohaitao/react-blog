e1c9c0bcbfabf235d4820cc12a446fc218266231
haohaitao <596336501@qq.com>
2022-07-01 10:35:35 +0800

    styles：完善页面样式

--- src/common/http.js
+++ src/common/http.js
@@ -1,24 +1,55 @@
-import axios from 'axios'
+/*
+ * @Description:
+ * @Author: pacino
+ * @Date: 2022-07-01 00:03:54
+ * @LastEditTime: 2022-07-01 10:33:57
+ * @LastEditors: pacino
+ */
+import axios from "axios";
+import { message } from "antd";
+const error = (msg) => {
+  message.error(msg);
+};
 
-function request(host,uri,method,data){
-    if(!data) data={}
+const service = axios.create({
+  timeout: 5000, // request timeout
+});
 
-    let r_url = (host) ? (host+uri) : uri
-    return axios({
-        method:method,
-        url:r_url,
-        data:data,
-        crossDomain: true,
-        headers:{
-            "Content-Type":'application/json',
-            "Accept":"application/json",
-        }
-    })
+// respone拦截器
+service.interceptors.response.use(
+  (response) => {
+    if (response.status === 200) {
+      return Promise.resolve(response);
+    } else {
+      error("数据异常，请稍后重试");
+      return Promise.reject(response || "error");
+    }
+  },
+  (error) => {
+    error("数据异常，请稍后重试");
+    return Promise.reject(error);
+  }
+);
+
+function request(host, uri, method, data) {
+  if (!data) data = {};
+
+  let r_url = host ? host + uri : uri;
+  return service({
+    method: method,
+    url: r_url,
+    data: data,
+    crossDomain: true,
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+  });
 }
 
-export function postJson(uri,data,host){
-    return request(host,uri,'post',data)
+export function postJson(uri, data, host) {
+  return request(host, uri, "post", data);
 }
-export function getJson(uri,data,host){
-    return request(host,uri,'get',data)
+export function getJson(uri, data, host) {
+  return request(host, uri, "get", data);
 }
--- src/view/Detail/style.js
+++ src/view/Detail/style.js
@@ -1,85 +1,93 @@
-import styled from 'styled-components'
+/*
+ * @Description:
+ * @Author: pacino
+ * @Date: 2022-07-01 00:03:54
+ * @LastEditTime: 2022-07-01 10:33:46
+ * @LastEditors: pacino
+ */
+import styled from "styled-components";
 
 export const ArticleContent = styled.article`
-    animation: fadeIn 0.6s linear;
-    max-width: 700px;
-    padding: 0 25px 30px;
-    margin: 0 auto;
-    background-color: #fff;
+  animation: fadeIn 0.6s linear;
+  max-width: 700px;
+  padding: 0 25px 30px;
+  margin: 0 auto;
+  background-color: #fff;
+  position: relative;
+  text-align: left;
+  font-size: 15px;
+  line-height: 32px;
+  margin-top: 90px;
+  min-height: 600px;
+  img {
+    max-width: 100%;
+    width: auto;
+    height: auto;
+    vertical-align: middle;
+    display: block;
+  }
+  .art-header {
+    padding-bottom: 12px;
+    border-bottom: 1px dashed #b7b7b7;
     position: relative;
-    text-align: left;
-    font-size: 15px;
-    line-height: 32px;
-    margin-top: 90px;
-    img{
-      max-width: 100%;
-      width: auto;
-      height: auto;
-      vertical-align: middle;
-      display: block;
-    }
-    .art-header {
-      padding-bottom: 12px;
-      border-bottom: 1px dashed #b7b7b7;
-      position: relative;
-    }
-
-    .header-info {
-      margin: 12px 0px 0;
-      color: #999;
-      font-size: 12px;
-    }
-    .header-info span{
-      padding-right:6px;
-    }
-
-    .article-info{
-      border-left: 2px solid #ccc;
-      background-color: #f6f8fa;
-      margin-top: 30px;
-      margin-bottom: 15px;
-      padding: 15px 0 15px 15px;
-      font-size: 14px;
-      margin-top:20px;
-    }
-    .content-footer {
-      border-top:1px dashed #eee;
-    }
-    a {
-      border-bottom: 1px solid #ccc;
-      color:#555;
-    }
-    .post-nav-l{
-      position: fixed;
-      left: 10px;
-      top: 45%;
-      font-size: 50px !important;
-      width: 50px;
-      z-index: 1;
-    }
-    .post-nav-l span{
-      font-size: 45px;
-      color: #b6b6b6;
-      text-align: center;
-    }
-    .post-nav-l span:hover {
-      color: #555;
-      cursor:pointer;
   }
-    .post-nav-r{
-      position: fixed;
-      right: 10px;
-      top: 45%;
-      width: 50px;
-      z-index: 1;
-    }
-    .post-nav-r span{
-      font-size: 45px;
-      color: #b6b6b6;
-      text-align: center;
-    }
-    .post-nav-r span:hover {
-      color: #555;
-      cursor:pointer;
+
+  .header-info {
+    margin: 12px 0px 0;
+    color: #999;
+    font-size: 12px;
   }
-`
+  .header-info span {
+    padding-right: 6px;
+  }
+
+  .article-info {
+    border-left: 2px solid #ccc;
+    background-color: #f6f8fa;
+    margin-top: 30px;
+    margin-bottom: 15px;
+    padding: 15px 0 15px 15px;
+    font-size: 14px;
+    margin-top: 20px;
+  }
+  .content-footer {
+    border-top: 1px dashed #eee;
+  }
+  a {
+    border-bottom: 1px solid #ccc;
+    color: #555;
+  }
+  .post-nav-l {
+    position: fixed;
+    left: 10px;
+    top: 45%;
+    font-size: 50px !important;
+    width: 50px;
+    z-index: 1;
+  }
+  .post-nav-l span {
+    font-size: 45px;
+    color: #b6b6b6;
+    text-align: center;
+  }
+  .post-nav-l span:hover {
+    color: #555;
+    cursor: pointer;
+  }
+  .post-nav-r {
+    position: fixed;
+    right: 10px;
+    top: 45%;
+    width: 50px;
+    z-index: 1;
+  }
+  .post-nav-r span {
+    font-size: 45px;
+    color: #b6b6b6;
+    text-align: center;
+  }
+  .post-nav-r span:hover {
+    color: #555;
+    cursor: pointer;
+  }
+`;
